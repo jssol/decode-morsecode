d2f8dec1e3376b7ccab9fc0e4f6f951e067adef2
bushmusi <bushera.mestofa@gmail.com>
2022-05-11 16:12:16 +0300

    Solve challenge

--- challenge.rb
+++ challenge.rb
@@ -37,3 +37,29 @@ MORSE_DICT = {
   '----.' => '9',
   '-----' => '0'
 }.freeze
+
+def decode_char(char)
+  return MORSE_DICT[char].capitalize()
+end
+
+def decode_word(word)
+  char_list = word.split(' ');
+  result = '';
+  char_list.each {
+    |x|
+    result +=decode_char(x)
+  }
+  return result
+end
+
+def decode(sentence)
+  word_list = sentence.split('   ');
+  result = ''
+  word_list.each{
+    |word|
+    result += decode_word(word)+' '
+  }
+  return result;
+end
+
+print(decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'))
